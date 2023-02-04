ebd72ec7e71b0aa3cd11ab7dffc465428a6d3a38
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 22:08:10 +0100

    5 - Change & Destroy Terraform Resources

--- main.tf
+++ main.tf
@@ -6,12 +6,18 @@ provider "aws" {
 
 resource "aws_vpc" "development-vpc" {
   cidr_block = "10.0.0.0/16"
+  tags       = {
+    Name = "development"
+  }
 }
 
 resource "aws_subnet" "development-subnet-1" {
   vpc_id            = aws_vpc.development-vpc.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = "eu-west-3a"
+  tags              = {
+    Name = "development-subnet-1"
+  }
 }
 
 data "aws_vpc" "existing_vpc" {
@@ -22,4 +28,7 @@ resource "aws_subnet" "dev-subnet-2" {
   vpc_id            = data.aws_vpc.existing_vpc.id
   cidr_block        = "172.31.48.0/20"
   availability_zone = "eu-west-3a"
+  tags              = {
+    Name = "dev-subnet-2"
+  }
 }
