15019db5b497ea849b713f6142914ee1c80be1d1
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 23:47:03 +0100

    12 - Automate Provisioning EC2 with Terraform - Part 1

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "avail_zone" {}
 variable "vpc_cider_block" {}
 variable "subnet_cider_block" {}
 variable "env_prefix" {}
+variable "my_ip" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cider_block
@@ -22,17 +23,6 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
-
-#resource "aws_route_table" "myapp-route-table" {
-#  vpc_id = aws_vpc.myapp-vpc.id
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.myapp-igw.id
-#  }
-#  tags = {
-#    Name : "${var.env_prefix}-rtb"
-#  }
-#}
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
@@ -53,7 +43,75 @@ resource "aws_default_route_table" "main-rtb" {
   }
 }
 
-#resource "aws_route_table_association" "a-rtb-subnet" {
-#  subnet_id      = aws_subnet.myapp-subnet-1.id
-#  route_table_id = aws_route_table.myapp-route-table.id
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      var.my_ip
+    ]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-default-sg"
+  }
+}
+#
+#resource "aws_security_group" "myapp-sg" {
+#  name   = "myapp-sg"
+#  vpc_id = aws_vpc.myapp-vpc.id
+#
+#  ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = [
+#      var.my_ip
+#    ]
+#  }
+#
+#  ingress {
+#    from_port   = 8080
+#    to_port     = 8080
+#    protocol    = "tcp"
+#    cidr_blocks = [
+#      "0.0.0.0/0"
+#    ]
+#  }
+#
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = [
+#      "0.0.0.0/0"
+#    ]
+#    prefix_list_ids = []
+#  }
+#
+#  tags = {
+#    Name : "${var.env_prefix}-sg"
+#  }
 #}
