cb212a75034e7aaba95951843a607233db46bb1b
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 22:00:44 +0100

    4 - Resources & Data Sources

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
 provider "aws" {
-  region     = "eu-central-1"
+  region     = "eu-west-3"
   access_key = ""
   secret_key = ""
 }
 
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "development-subnet-1" {
+  vpc_id            = aws_vpc.development-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "eu-west-3a"
+}
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "eu-west-3a"
+}
