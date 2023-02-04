e723e40221842cc366886762c9385c662300649b
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 00:59:16 +0100

    15 - Provisioners in Terraform

--- main.tf
+++ main.tf
@@ -135,6 +135,7 @@ resource "aws_instance" "myapp-server" {
         inline = [
           "sudo yum update -y",
           "sudo yum install -y docker",
+          "sudo chmod 666 /var/run/docker.sock",
           "sudo systemctl start docker",
           "sudo systemctl enable docker",
           "sudo usermod -a -G docker ec2-user",
