a1296071c992800faacf46f5265d16fd4a80bb86
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 10:37:07 +0100

    Set master to correct in the course

--- main.tf
+++ main.tf
@@ -2,127 +2,53 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable "avail_zone" {}
-variable "vpc_cider_block" {}
-variable "subnet_cider_block" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
+variable "avail_zone" {
+  description = "availability zone"
+  default     = "eu-west-3a"
+}
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cider_block
+variable "cider_block" {
+  description = "subnet cidr block"
+  type        = list(object({
+    cidr_block = string
+    name       = string
+  }))
+}
+
+variable "environment" {
+  description = "deployment environment"
+  default     = "development"
+  type        = string
+}
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = var.cider_block[0].cidr_block
   tags       = {
-    Name = "${var.env_prefix}-vpc"
+    Name        = var.cider_block[0].name
+    Environment = var.environment
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cider_block
+resource "aws_subnet" "development-subnet-1" {
+  vpc_id            = aws_vpc.development-vpc.id
+  cidr_block        = var.cider_block[1].cidr_block
   availability_zone = var.avail_zone
   tags              = {
-    Name = "${var.env_prefix}-subnet-1"
+    Name        = var.cider_block[1].name
+    Environment = var.environment
   }
 }
 
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags   = {
-    Name : "${var.env_prefix}-igw"
-  }
+data "aws_vpc" "existing_vpc" {
+  default = true
 }
 
-resource "aws_default_route_table" "main-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-main-rtb"
-  }
-}
-
-resource "aws_default_security_group" "default-sg" {
-  vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [
-      var.my_ip
-    ]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name : "${var.env_prefix}-default-sg"
-  }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami           = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = var.instance_type
-
-  subnet_id              = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [
-    aws_default_security_group.default-sg.id
-  ]
+resource "aws_subnet" "development-subnet-2" {
+  vpc_id            = data.aws_vpc.existing_vpc.id
+  cidr_block        = var.cider_block[2].cidr_block
   availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name                    = aws_key_pair.ssh-key.key_name
-
-  tags = {
-    Name : "${var.env_prefix}-server"
+  tags              = {
+    Name        = var.cider_block[2].name
+    Environment = var.environment
   }
-
-  user_data = file("entry-script.sh")
-}
-
-output "aws_ami_id" {
-value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "aws_public_ip" {
-value = aws_instance.myapp-server.public_ip
 }
--- terraform-dev.tfvars
+++ terraform-dev.tfvars
@@ -1,7 +1,7 @@
-avail_zone          = "eu-west-3b"
-vpc_cider_block     = "10.0.0.0/16"
-subnet_cider_block  = "10.0.100.0/24"
-env_prefix          = "dev"
-my_ip               = "0.0.0.0/0"
-instance_type       = "t2.micro"
-public_key_location = "/Users/joeriabbo/.ssh/id_rsa.pub"
+cider_block = [
+  { cidr_block = "10.0.0.0/16", name = "dev-vpc" },
+  { cidr_block = "10.0.50.0/24", name = "development-subnet-1" },
+  { cidr_block = "172.31.48.0/20", name = "development-subnet-2" }
+]
+
+environment = "development"
